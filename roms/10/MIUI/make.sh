f73d1258c02a722830cbf1bd7ce53acf52114dcf
Krzysiek <kryzo123456@gmail.com>
2020-07-16 17:17:58 +0200

    MIUI_Daisy_v1

--- roms/10/MIUI/make.sh
+++ roms/10/MIUI/make.sh
@@ -3,33 +3,35 @@
 systempath=$1
 thispath=`cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd`
 
-# build.prop
-#echo "ro.bluetooth.library_name=libbluetooth_qti.so" >> $1/build.prop
+sed -i "/miui.notch/d" $1/build.prop
 
-# drop finddevice, needs to be done before copying system files
-rm -rf $1/priv-app/FindDevice
+# build.prop
+echo "ro.bluetooth.library_name=libbluetooth_qti.so" >> $1/build.prop
+
 
 # Copy system files
 rsync -ra $thispath/system/ $systempath
 
 #fix systemui crash because of FOD
-echo "ro.hardware.fp.fod=true" >> $1/build.prop
-echo "persist.vendor.sys.fp.fod.location.X_Y=445,1260" >> $1/build.prop
-echo "persist.vendor.sys.fp.fod.size.width_height=190,190" >> $1/build.prop
 echo "DEVICE_PROVISIONED=1" >> $1/build.prop
 
-# drop dirac
-rm -rf $1/priv-app/DiracAudioControlService
-# drop FingerprintExtensionService
-rm -rf $1/app/FingerprintExtensionService
+#fix notch
+echo "ro.miui.notch=1" >> $1/build.prop
+
+#fix BT
+echo "persist.bluetooth.bluetooth_audio_hal.disabled=true" >> $1/build.prop
+echo "persist.bluetooth.bluetooth_audio_hal.enabled=false" >> $1/build.prop
+echo "persist.vendor.btstack.a2dp_offload_cap=sbc-aptx-aptxhd-aac" >> $1/build.prop
+echo "vendor.bluetooth.soc=cherokee" >> $1/build.prop
+
+
 # drop nfc
 rm -rf $1/app/NQNfcNci
 
 cat $thispath/rw-system.add.sh >> $1/bin/rw-system.sh
 
-sed -i 's/<bool name="support_round_corner">true/<bool name="support_round_corner">false/' $1/etc/device_features/*
+sed -i 's/<bool name="support_round_corner">true/<bool name="support_round_corner">true/' $1/etc/device_features/*
 
-sed -i "/miui.notch/d" $1/build.prop
 
 # Wifi fix
 cp -fpr $thispath/bin/* $1/bin/
